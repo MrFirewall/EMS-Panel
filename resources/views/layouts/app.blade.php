ba25288b620073b305f139246d471068bfce1ec9
MrFirewall <32974726+MrFirewall@users.noreply.github.com>
2025-10-16 16:18:37 +0200

    Hide already selected options in dropdown for improved clarity

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -36,6 +36,10 @@
             background-color: #454d55;
             color: #fff;
         }
+        /* NEU: Versteckt bereits ausgewählte Optionen in der Dropdown-Liste für eine bessere Übersicht */
+        .select2-results__option[aria-selected=true] {
+            display: none;
+        }
     </style>
     @stack('styles')
 </head>
