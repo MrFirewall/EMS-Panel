5e3958cd009c8f8c2918e0b782f88709fc546061
MrFirewall <32974726+MrFirewall@users.noreply.github.com>
2025-10-17 11:02:29 +0200

    chore: Normalize line endings

--- resources/views/citizens/_form.blade.php
+++ resources/views/citizens/_form.blade.php
@@ -71,16 +71,80 @@
 </div>
 <div class="form-group">
     <label for="allergies">Allergien & Unverträglichkeiten</label>
-    <textarea class="form-control @error('allergies') is-invalid @enderror" id="allergies" name="allergies" rows="3">{{ old('allergies', $citizen->allergies ?? '') }}</textarea>
+    <div class="input-group">
+        <textarea class="form-control @error('allergies') is-invalid @enderror" id="allergies" name="allergies" rows="3">{{ old('allergies', $citizen->allergies ?? '') }}</textarea>
+        <div class="input-group-append">
+            <button class="btn btn-outline-secondary" type="button" data-toggle="modal" data-target="#allergiesModal">Vorlage...</button>
+        </div>
+    </div>
     @error('allergies')<div class="invalid-feedback">{{ $message }}</div>@enderror
 </div>
+
 <div class="form-group">
     <label for="preexisting_conditions">Vorerkrankungen</label>
-    <textarea class="form-control @error('preexisting_conditions') is-invalid @enderror" id="preexisting_conditions" name="preexisting_conditions" rows="3">{{ old('preexisting_conditions', $citizen->preexisting_conditions ?? '') }}</textarea>
+    <div class="input-group">
+        <textarea class="form-control @error('preexisting_conditions') is-invalid @enderror" id="preexisting_conditions" name="preexisting_conditions" rows="3">{{ old('preexisting_conditions', $citizen->preexisting_conditions ?? '') }}</textarea>
+        <div class="input-group-append">
+             <button class="btn btn-outline-secondary" type="button" data-toggle="modal" data-target="#conditionsModal">Vorlage...</button>
+        </div>
+    </div>
     @error('preexisting_conditions')<div class="invalid-feedback">{{ $message }}</div>@enderror
 </div>
 <div class="form-group">
     <label for="notes">Allgemeine Notizen</label>
     <textarea class="form-control @error('notes') is-invalid @enderror" id="notes" name="notes" rows="4">{{ old('notes', $citizen->notes ?? '') }}</textarea>
     @error('notes')<div class="invalid-feedback">{{ $message }}</div>@enderror
+</div>
+<div class="modal fade" id="allergiesModal" tabindex="-1" role="dialog" aria-labelledby="allergiesModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="allergiesModalLabel">Allergien aus Vorlage auswählen</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        {{-- Passe diese Liste nach deinen RP-Bedürfnissen an --}}
+        <div class="form-check"><input class="form-check-input" type="checkbox" value="Pollenallergie (Heuschnupfen)"> Pollenallergie (Heuschnupfen)</div>
+        <div class="form-check"><input class="form-check-input" type="checkbox" value="Hausstaubmilbenallergie"> Hausstaubmilbenallergie</div>
+        <div class="form-check"><input class="form-check-input" type="checkbox" value="Tierhaarallergie (z.B. Katzen, Hunde)"> Tierhaarallergie</div>
+        <div class="form-check"><input class="form-check-input" type="checkbox" value="Schimmelpilzallergie"> Schimmelpilzallergie</div>
+        <div class="form-check"><input class="form-check-input" type="checkbox" value="Nahrungsmittelallergie (z.B. Nüsse, Laktose)"> Nahrungsmittelallergie</div>
+        <div class="form-check"><input class="form-check-input" type="checkbox" value="Penicillin-Allergie"> Penicillin-Allergie</div>
+        <div class="form-check"><input class="form-check-input" type="checkbox" value="Insektengiftallergie (Bienen, Wespen)"> Insektengiftallergie</div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Abbrechen</button>
+        <button type="button" class="btn btn-primary" id="selectAllergiesBtn">Auswahl übernehmen</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="modal fade" id="conditionsModal" tabindex="-1" role="dialog" aria-labelledby="conditionsModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="conditionsModalLabel">Vorerkrankungen aus Vorlage auswählen</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        {{-- Passe diese Liste nach deinen RP-Bedürfnissen an --}}
+        <div class="form-check"><input class="form-check-input" type="checkbox" value="Asthma bronchiale"> Asthma bronchiale</div>
+        <div class="form-check"><input class="form-check-input" type="checkbox" value="Bluthochdruck (Hypertonie)"> Bluthochdruck (Hypertonie)</div>
+        <div class="form-check"><input class="form-check-input" type="checkbox" value="Diabetes Mellitus Typ 1"> Diabetes Mellitus Typ 1</div>
+        <div class="form-check"><input class="form-check-input" type="checkbox" value="Diabetes Mellitus Typ 2"> Diabetes Mellitus Typ 2</div>
+        <div class="form-check"><input class="form-check-input" type="checkbox" value="Herzerkrankung (z.B. KHK)"> Herzerkrankung (z.B. KHK)</div>
+        <div class="form-check"><input class="form-check-input" type="checkbox" value="Chronische Migräne"> Chronische Migräne</div>
+        <div class="form-check"><input class="form-check-input" type="checkbox" value="Epilepsie"> Epilepsie</div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Abbrechen</button>
+        <button type="button" class="btn btn-primary" id="selectConditionsBtn">Auswahl übernehmen</button>
+      </div>
+    </div>
+  </div>
 </div>
