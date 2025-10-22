5eb4ce748b192d66a2ad7d5f5b25dd9a1140501d
MrFirewall <32974726+MrFirewall@users.noreply.github.com>
2025-10-22 23:53:25 +0200

    ✨ feat(admin): improve notification rules view structure
    - enhance layout consistency for create and edit notification rule views
    - update breadcrumbs for better navigation
    - organize content using AdminLTE components for a cleaner UI
    - ensure CSRF token and PUT method are correctly included for form submissions

--- resources/views/admin/notification-rules/create.blade.php
+++ resources/views/admin/notification-rules/create.blade.php
@@ -1,34 +1,43 @@
-@extends('layouts.app')
+@extends('layouts.app') {{-- Ersetze 'layouts.app' durch dein Admin-Layout --}}
+
 @section('title', 'Neue Benachrichtigungsregel erstellen')
+
 @section('content')
-     <div class="content-header">
+    {{-- AdminLTE Content Header --}}
+    <div class="content-header">
         <div class="container-fluid">
             <div class="row mb-2">
                 <div class="col-sm-6">
-                    <h1 class="m-0"><i class="fas fa-plus-circle nav-icon"></i> Neue Benachrichtigungsregel</h1>
+                    <h1 class="m-0"><i class="fas fa-plus-circle"></i> Neue Benachrichtigungsregel</h1>
                 </div>
                 <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-right">
                         <li class="breadcrumb-item"><a href="{{ route('dashboard') }}">Dashboard</a></li>
-                         <li class="breadcrumb-item"><a href="{{ route('admin.notification-rules.index') }}">Benachrichtigungsregeln</a></li>
-                         <li class="breadcrumb-item active">Erstellen</li>
+                        <li class="breadcrumb-item"><a href="{{ route('admin.notification-rules.index') }}">Benachrichtigungsregeln</a></li>
+                        <li class="breadcrumb-item active">Neu</li>
                     </ol>
                 </div>
             </div>
         </div>
-     </div>
-     <div class="content">
-         <div class="container-fluid">
-             <form action="{{ route('admin.notification-rules.store') }}" method="POST">
-                 <div class="card card-primary card-outline">
-                     <div class="card-header">
-                         <h3 class="card-title">Regeldetails</h3>
-                     </div>
-                     {{-- Include form partial --}}
-                     @include('admin.notification-rules._form', ['notificationRule' => null])
-                 </div>
-             </form>
-         </div>
-     </div>
+    </div>
+
+    {{-- Main Content --}}
+    <div class="content">
+        <div class="container-fluid">
+            {{-- Formular beginnt hier --}}
+            <form action="{{ route('admin.notification-rules.store') }}" method="POST">
+                @csrf {{-- WICHTIG: CSRF Token --}}
+                <div class="card card-primary card-outline">
+                    <div class="card-header">
+                        <h3 class="card-title">Regeldetails</h3>
+                    </div>
+                    {{-- Include form partial, übergibt null, da keine Regel existiert --}}
+                    @include('admin.notification-rules._form', ['notificationRule' => null])
+                </div>
+            </form>
+            {{-- Formular endet hier --}}
+        </div>
+    </div>
 @endsection
 
+{{-- Select2 JS wird aus dem _form Partial gepusht --}}
--- resources/views/admin/notification-rules/edit.blade.php
+++ resources/views/admin/notification-rules/edit.blade.php
@@ -1,41 +1,44 @@
-@extends('layouts.app')
+@extends('layouts.app') {{-- Ersetze 'layouts.app' durch dein Admin-Layout --}}
+
 @section('title', 'Benachrichtigungsregel bearbeiten')
 
-@push('styles')
-    <link rel="stylesheet" href="[https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css](https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css)" />
-    <link rel="stylesheet" href="[https://cdn.jsdelivr.net/npm/@ttskch/select2-bootstrap4-theme@x.x.x/dist/select2-bootstrap4.min.css](https://cdn.jsdelivr.net/npm/@ttskch/select2-bootstrap4-theme@x.x.x/dist/select2-bootstrap4.min.css)"> {{-- Passe ggf. die Version an --}}
-@endpush
-
 @section('content')
-     <div class="content-header">
-         <div class="container-fluid">
-             <div class="row mb-2">
-                 <div class="col-sm-6">
-                     <h1 class="m-0"><i class="fas fa-edit nav-icon"></i> Benachrichtigungsregel bearbeiten</h1>
-                 </div>
-                 <div class="col-sm-6">
-                     <ol class="breadcrumb float-sm-right">
-                         <li class="breadcrumb-item"><a href="{{ route('dashboard') }}">Dashboard</a></li>
-                          <li class="breadcrumb-item"><a href="{{ route('admin.notification-rules.index') }}">Benachrichtigungsregeln</a></li>
-                          <li class="breadcrumb-item active">Bearbeiten #{{ $notificationRule->id }}</li>
-                     </ol>
-                 </div>
-             </div>
-         </div>
-     </div>
-     <div class="content">
-         <div class="container-fluid">
-             <form action="{{ route('admin.notification-rules.update', $notificationRule) }}" method="POST">
-                 @method('PUT')
-                 <div class="card card-primary card-outline">
-                     <div class="card-header">
-                         <h3 class="card-title">Regel #{{ $notificationRule->id }} Details</h3>
-                     </div>
-                      {{-- Include form partial --}}
-                     @include('admin.notification-rules._form', ['notificationRule' => $notificationRule])
-                 </div>
-             </form>
-         </div>
-     </div>
+    {{-- AdminLTE Content Header --}}
+    <div class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1 class="m-0"><i class="fas fa-edit"></i> Benachrichtigungsregel bearbeiten</h1>
+                </div>
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item"><a href="{{ route('dashboard') }}">Dashboard</a></li>
+                        <li class="breadcrumb-item"><a href="{{ route('admin.notification-rules.index') }}">Benachrichtigungsregeln</a></li>
+                        <li class="breadcrumb-item active">Bearbeiten</li>
+                    </ol>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    {{-- Main Content --}}
+    <div class="content">
+        <div class="container-fluid">
+             {{-- Formular beginnt hier --}}
+            <form action="{{ route('admin.notification-rules.update', $notificationRule) }}" method="POST">
+                @csrf {{-- WICHTIG: CSRF Token --}}
+                @method('PUT') {{-- WICHTIG: Methode für Update --}}
+                <div class="card card-primary card-outline">
+                    <div class="card-header">
+                        <h3 class="card-title">Regeldetails bearbeiten</h3>
+                    </div>
+                    {{-- Include form partial, übergibt die zu bearbeitende Regel --}}
+                    @include('admin.notification-rules._form', ['notificationRule' => $notificationRule])
+                </div>
+            </form>
+             {{-- Formular endet hier --}}
+        </div>
+    </div>
 @endsection
 
+{{-- Select2 JS wird aus dem _form Partial gepusht --}}
