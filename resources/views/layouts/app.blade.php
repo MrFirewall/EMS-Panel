3d176663d7bcd33324ecdafa0b52556c49d06f5c
MrFirewall <32974726+MrFirewall@users.noreply.github.com>
2025-10-22 19:50:30 +0200

    🐛 fix(notifications): correct private channel event name
    - changed 'new.ems.notification' to '.new.ems.notification' to correctly match the broadcast event name for private channels.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -474,7 +474,7 @@
         console.log('[DEBUG] 7. Listener für Benachrichtigungen auf privatem Kanal aktiviert.');
         window.Echo.private(`users.{{ Auth::id() }}`) 
             // Lauscht auf den im Backend definierten broadcastAs-Namen
-            .listen('new.ems.notification', (e) => { 
+            .listen('.new.ems.notification', (e) => { 
                 console.log('--- ECHTZEIT EVENT EMPFANGEN ---');
                 console.log('[DEBUG] 8. Benachrichtigung über .listen() erhalten!', e);
                 // Lädt das Dropdown nur, wenn ein Event eintrifft
