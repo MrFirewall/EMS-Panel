d21d1034b9124c432d6a9becb0e4285bcf55b2e3
MrFirewall <32974726+MrFirewall@users.noreply.github.com>
2025-10-25 01:22:22 +0200

    Update Exam.php

--- app/Models/Exam.php
+++ app/Models/Exam.php
@@ -6,9 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 class Exam extends Model
 {
     use HasFactory;
-    protected $fillable = ['training_module_id', 'title', 'description', 'pass_mark'];
+    protected $fillable = ['title', 'description', 'pass_mark'];
 
-    public function trainingModule() { return $this->belongsTo(TrainingModule::class); }
     public function questions() { return $this->hasMany(Question::class); }
     public function attempts() { return $this->hasMany(ExamAttempt::class); }
 }
