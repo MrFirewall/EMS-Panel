13cf02687c574727df84ae46d009a12f093ffe1e
MrFirewall <32974726+MrFirewall@users.noreply.github.com>
2025-10-25 02:33:36 +0200

    Update result.blade.php

--- resources/views/exams/result.blade.php
+++ resources/views/exams/result.blade.php
@@ -13,7 +13,7 @@
             </div>
             <div class="col-sm-6">
                 <ol class="breadcrumb float-sm-right">
-                    <li class="breadcrumb-item"><a href="{{ route('admin.dashboard') ?? route('dashboard') }}">Dashboard</a></li>
+                    <li class="breadcrumb-item"><a href="{{ route('dashboard') ?? route('dashboard') }}">Dashboard</a></li>
                     <li class="breadcrumb-item"><a href="{{ route('admin.exams.attempts.index') }}">Prüfungsversuche</a></li>
                     <li class="breadcrumb-item active">Ergebnis / Bewertung</li>
                 </ol>
