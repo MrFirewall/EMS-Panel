87c53488f5daa2e47335f8ed72123961999ec6b4
MrFirewall <32974726+MrFirewall@users.noreply.github.com>
2025-11-01 12:05:37 +0100

    💄 style(profile): shorten german titles in profile views
    - "Prüfungsergebnisse" changed to "Prüfungen"
    - "Zugewiesene Module" changed to "Module"

--- resources/views/profile/partials/training-modules.blade.php
+++ resources/views/profile/partials/training-modules.blade.php
@@ -1,6 +1,6 @@
-<div class="card shadow-sm mb-4">
-    <div class="card-header bg-primary text-white">
-        <h5 class="mb-0">Zugewiesene Module</h5>
+<div class="card card-primary card-outline mb-4">
+    <div class="card-header">
+        <h5 class="mb-0">Module</h5>
     </div>
     <div class="card-body">
         <table class="table table-striped table-hover mb-0">
